bf62054fbc83bb3ee6437c73d27fea3340fc19f4
Brendan Klayman <bklayma1@binghamton.edu>
2021-02-15 12:02:45 -0500

    Added languages to Hello World

--- Hello World/Hello World/ViewController.m
+++ Hello World/Hello World/ViewController.m
@@ -19,10 +19,49 @@
     // Do any additional setup after loading the view.
 }
 
-- (IBAction) clickedTheButton:(id)sender
+- (IBAction) spanishButtonClicked:(id)sender
 {
-    [myLabel setText:@"The button was clicked"]; //myLabel.setText() is the Java version
-    
+    [myLabel setText:@"Hola Mundo"]; //myLabel.setText() is the Java version
+}
+
+- (IBAction) englishButtonClicked:(id)sender
+{
+    [myLabel setText:@"Hello World"];
+}
+
+- (IBAction) frenchButtonClicked:(id)sender
+{
+    [myLabel setText:@"Bonjour le Monde"];
+}
+
+- (IBAction) italianButtonClicked:(id)sender
+{
+    [myLabel setText:@"Ciao Mondo"];
+}
+
+- (IBAction) dutchButtonClicked:(id)sender
+{
+    [myLabel setText:@"Hallo Wereld"];
+}
+
+- (IBAction) finnishButtonClicked:(id)sender
+{
+    [myLabel setText:@"Hei Maailma"];
+}
+
+- (IBAction) latinButtonClicked:(id)sender
+{
+    [myLabel setText:@"Salve Orbis Terrarum"];
+}
+
+- (IBAction) irishButtonClicked:(id)sender
+{
+    [myLabel setText:@"Dia Duit an Domhan"];
+}
+
+- (IBAction) welshButtonClicked:(id)sender
+{
+    [myLabel setText:@"Helo Byd"];
 }
 
 @end
