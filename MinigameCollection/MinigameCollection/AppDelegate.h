b7a24471532e5593eb172d73190707596379c687
Brendan Klayman <bklayma1@binghamton.edu>
2021-02-17 10:31:13 -0500

    Started hangman in minigame collection

---
+++ MinigameCollection/MinigameCollection/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MinigameCollection
+//
+//  Created by Brendan Klayman on 2/17/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MinigameCollection/MinigameCollection/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MinigameCollection
+//
+//  Created by Brendan Klayman on 2/17/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
