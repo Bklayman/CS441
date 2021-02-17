2fa214763a8d92225cd63fccfb2b716004e08053
Brendan Klayman <bklayma1@binghamton.edu>
2021-02-17 10:39:33 -0500

    Started file reading for hangman in minigame collection

--- MinigameCollection/MinigameCollection/ViewController.m
+++ MinigameCollection/MinigameCollection/ViewController.m
@@ -16,6 +16,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSString* hangmanWordListPath = [[NSBundle mainBundle] pathForResource:@"wordlist" ofType:@"txt"];
+    NSMutableArray *hangmanWordsArray = [NSMutableArray array];
+    for(int i = 0; i < 10000; i++){
+        //TODO Add words to hangmanWordsArray
+    }
 }
 
 
